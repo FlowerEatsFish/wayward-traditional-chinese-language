698a4da1c1ea7fc5b59bad8094f15790db6ef31c
Eating-Fish Flower <flower.eating.fish@gmail.com>
2017-01-03 23:30:12 +0800

    Creating a prototype of translation file.
    Traditional Chinese translation for Wayward on version beta 2.2.3.

---
+++ TraditionalChinese.ts
@@ -0,0 +1,41 @@
+﻿/// <reference path="languages.ts" />
+/// <reference path="messages.ts" />
+/// <reference path="tileevents.ts" />
+/// <reference path="milestones.ts" />
+
+
+// UI
+
+// Messages
+// NOTE: Style guide
+// No contractions for in-game messages (other UI/dialogs can use them)
+// Past tense for all messages that appear when/during taking a turn
+// Present tense for all messages that appear when no turn is passed
+// For the style guide of the enum names, check enums.ts
+
+// Items
+
+// Groups
+
+// Doodads
+
+// Doodad groups
+
+// Uses
+
+// Terrain/tiles
+
+// Events
+
+// On Equips
+
+// Creatures
+
+// Unique corpse types
+
+// Skills
+
+// Milestones
+
+// Hints
+
